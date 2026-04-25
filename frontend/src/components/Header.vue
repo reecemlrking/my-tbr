07fc08fee7c2b9903db130d308ee55bb9849c65e
Reece King <reeceking9922@gmail.com>
2026-04-25 15:29:25 -0600

    added BookList and BookContents components

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -1,7 +1,8 @@
 <script setup lang="ts">
 import { ref } from 'vue';
 
-const title = ref("Title");
+const title = ref("My TBR");
+const modelSearch = ref("");
 
 </script>
 
@@ -17,10 +18,14 @@ const title = ref("Title");
 </style>
 
 <template>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
   <div class="header">
     <span>
       <h1 class="header-text">{{ title }}</h1>
-
+      <input v-model="modelSearch" placeholder="search" />
+      <button class="icon"><i class="fa fa-search"></i></button>
+      <p>{{ modelSearch }}</p>
     </span>
   </div>
 </template>
